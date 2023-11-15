394c42bfcaf29a30da912c2394ecd4db086a3a77
unknown <robertishimwe0@gmail.com>
2023-11-15 12:34:02 +0000

    set db

--- src/config/dbase.js
+++ src/config/dbase.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 const Sequelize = require("sequelize")
 module.exports = new Sequelize('defaultdb', 'avnadmin', 'AVNS_EfUj-VLFHelXr_GugKm', {
     host: 'test-robertishimwe0-5cb2.a.aivencloud.com',
+    port: 26939,
     dialect:'mysql',
+    dialectOptions: {
+      ssl: {
+        ca: path.join(__dirname, 'ca.pem'),
+        rejectUnauthorized: false,
+      }
+    },
   
     pool: {
       max: 40,
