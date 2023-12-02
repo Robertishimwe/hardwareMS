15b13115413744fd04656bf04a45b25649612ddb
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-12-02 16:59:13 +0000

    fixed inventory transuction

--- src/validations/inventoryValidation.js
+++ src/validations/inventoryValidation.js
@@ -1,6 +1,58 @@
+// const Joi = require("joi");
+
+// const stockSchema = Joi.object({
+//   productId: Joi.number().integer().positive().required().messages({
+//     "number.base": "{{#label}} must be a number",
+//     "number.integer": "{{#label}} must be an integer",
+//     "number.positive": "{{#label}} must be a positive number",
+//     "any.required": "{{#label}} is required",
+//   }),
+
+//   amount: Joi.number().integer().min(1).required().messages({
+//     "number.base": "{{#label}} must be a number",
+//     "number.integer": "{{#label}} must be an integer",
+//     "number.min": "{{#label}} must be at least 1",
+//     "any.required": "{{#label}} is required",
+//   }),
+// });
+
+// class StockValidation {
+//   static verifyStockAddition = (req, res, next) => {
+//     const { error } = stockSchema.validate(req.body);
+//     if (error) {
+//       return res.status(422).json({
+//         error: error.details[0].message.replace(/["'`]+/g, ""),
+//       });
+//     }
+//     next();
+//   };
+
+//   static verifyStockDeduction = (req, res, next) => {
+//     const { error } = stockSchema.validate(req.body);
+//     if (error) {
+//       return res.status(422).json({
+//         error: error.details[0].message.replace(/["'`]+/g, ""),
+//       });
+//     }
+
+//     // Custom validation for amount deduction (if any specific conditions arise in future)
+//     // Custom validation logic can be added here for specific deduction rules
+
+//     next();
+//   };
+// }
+
+// module.exports = StockValidation;
+
+
+
+
+
+
+
 const Joi = require("joi");
 
-const stockSchema = Joi.object({
+const transactionSchema = Joi.object({
   productId: Joi.number().integer().positive().required().messages({
     "number.base": "{{#label}} must be a number",
     "number.integer": "{{#label}} must be an integer",
@@ -16,9 +68,11 @@ const stockSchema = Joi.object({
   }),
 });
 
+const transactionsSchema = Joi.array().items(transactionSchema).min(1);
+
 class StockValidation {
   static verifyStockAddition = (req, res, next) => {
-    const { error } = stockSchema.validate(req.body);
+    const { error } = transactionsSchema.validate(req.body);
     if (error) {
       return res.status(422).json({
         error: error.details[0].message.replace(/["'`]+/g, ""),
@@ -28,7 +82,7 @@ class StockValidation {
   };
 
   static verifyStockDeduction = (req, res, next) => {
-    const { error } = stockSchema.validate(req.body);
+    const { error } = transactionsSchema.validate(req.body);
     if (error) {
       return res.status(422).json({
         error: error.details[0].message.replace(/["'`]+/g, ""),
@@ -43,3 +97,4 @@ class StockValidation {
 }
 
 module.exports = StockValidation;
+
