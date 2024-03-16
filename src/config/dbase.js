dd4bb2c6673ba4864ba12064cfeb3c3086c47b37
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-03-16 12:28:41 +0000

    change to preproduction

--- src/config/dbase.js
+++ src/config/dbase.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 const Sequelize = require("sequelize")
-module.exports = new Sequelize('defaultdb', 'avnadmin', 'AVNS_EfUj-VLFHelXr_GugKm', {
+
+// module.exports = new Sequelize('defaultdb', 'avnadmin', 'AVNS_EfUj-VLFHelXr_GugKm', {
+module.exports = new Sequelize('preproduction', 'avnadmin', 'AVNS_EfUj-VLFHelXr_GugKm', {
     host: 'test-robertishimwe0-5cb2.a.aivencloud.com',
     port: 26939,
     dialect:'mysql',
