979a6d3f56f19ace888f201b153243c28b8195c4
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-03-16 12:36:54 +0000

    change to preproduction

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ if (cluster.isMaster) {
   db.authenticate()
     .then(() => {
       console.log('Database synced');
-      createAdminUser();
+      // createAdminUser();
       app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT} in worker ${process.pid}`);
       });
