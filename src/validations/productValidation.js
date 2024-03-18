aec5fa794e81163d29b7cb637a489d5e81da0f02
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-03-18 14:26:44 +0000

    fixed error

--- src/validations/productValidation.js
+++ src/validations/productValidation.js
@@ -10,40 +10,40 @@ const productSchema = Joi.object({
   }),
 
   description: Joi.string().trim().min(10).max(200).required().messages({
-    "string.base": "{{#label}} must be a string",
     "string.empty": "{{#label}} cannot be empty",
+    "string.base": "{{#label}} must be a string",
     "string.min": "{{#label}} should have at least {{#limit}} characters",
     "string.max": "{{#label}} should not exceed {{#limit}} characters",
     "any.required": "{{#label}} is required",
   }),
 
   category: Joi.number().integer().positive().required().messages({
-    "number.base": "{{#label}} must be a number",
     "string.empty": "{{#label}} cannot be empty",
+    "number.base": "{{#label}} must be a number",
     "string.min": "{{#label}} should have at least {{#limit}} characters",
     "string.max": "{{#label}} should not exceed {{#limit}} characters",
     "any.required": "{{#label}} is required",
   }),
 
   supplier_id: Joi.number().integer().positive().required().messages({
+    "any.required": "{{#label}} is required",
     "number.base": "{{#label}} must be a number",
     "number.integer": "{{#label}} must be an integer",
     "number.positive": "{{#label}} must be a positive number",
-    "any.required": "{{#label}} is required",
   }),
 
   unit_id: Joi.number().integer().positive().required().messages({
+    "any.required": "{{#label}} is required",
     "number.base": "{{#label}} must be a number",
     "number.integer": "{{#label}} must be an integer",
     "number.positive": "{{#label}} must be a positive number",
-    "any.required": "{{#label}} is required",
   }),
 
   price: Joi.number().positive().precision(2).required().messages({
+    "any.required": "{{#label}} is required",
     "number.base": "{{#label}} must be a number",
     "number.positive": "{{#label}} must be a positive number",
     "number.precision": "{{#label}} should have a maximum of 2 decimal places",
-    "any.required": "{{#label}} is required",
   }),
 });
 
