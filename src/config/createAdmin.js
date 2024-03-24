8342019d12871fcb9f0873e99109433fcfd87ae7
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-03-24 10:08:46 +0000

    added check admin and manager route

--- src/config/createAdmin.js
+++ src/config/createAdmin.js
@@ -3,28 +3,42 @@ const UserService = require('../services/user.service')
 const Protection = require('../helpers/encreption');
 
 async function createAdminUser() {
-    const adminuser = UserService.findUser({ role: "admin" })
-    if (adminuser) {
-       
-        console.log("admin already exist")
-        return
-    };
-    try {
-        const newUser = await User.create({
-            firstName: "admin",
-            lastName: "admin",
-            email: "admin@a1dmin.com",
-            phone: "0787885197",
-            role: "admin",
-            password: await Protection.hashPassword("123")
-        });
-        console.log('New user created:', newUser.toJSON());
+    const adminUser = await UserService.findUser({  email: "admin@a1dmin.com" });
+    const systemUser = await UserService.findUser({ email: "robert@ishimwe.rw" });
 
-        return newUser;
+    if (adminUser && systemUser) {
+        console.log("Admin and system users already exist");
+        return;
+    }
+
+    try {
+        if (!adminUser) {
+            const newAdminUser = await User.create({
+                firstName: "admin",
+                lastName: "admin",
+                email: "admin@a1dmin.com",
+                phone: "0787885197",
+                role: "admin",
+                password: await Protection.hashPassword("123")
+            });
+            console.log('New admin user created:', newAdminUser.toJSON());
+        }
+
+        if (!systemUser) {
+            const newSystemUser = await User.create({
+                firstName: "system",
+                lastName: "user",
+                email: "robert@ishimwe.rw",
+                phone: "0787885197", // You can replace this with the actual phone number
+                role: "admin", // You can replace this with the actual role
+                password: await Protection.hashPassword("123") // You can replace this with the actual password
+            });
+            console.log('New system user created:', newSystemUser.toJSON());
+        }
     } catch (error) {
         console.error('Error creating user:', error);
         throw error; // You might want to handle this error in a different way
     }
 }
 
-module.exports = createAdminUser
+module.exports = createAdminUser;
