9aa496d0f9633a739fb164ab2d9a424b7f4a79dc
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-04-08 15:00:01 +0000

    added supplier to transaction schema

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -6,7 +6,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.patch("/add",checkAdminOrManager, stockValidation.verifyStockAddition, inventoryController.increment)
+// router.patch("/add",checkAdminOrManager, stockValidation.verifyStockAddition, inventoryController.increment)
+router.patch("/add",checkAdminOrManager, inventoryController.increment)
 router.patch('/dedact', stockValidation.verifyStockDeduction, inventoryController.decrement)
 router.get("/getAllStock",  inventoryController.getAllStock)
 // router.post("/login", authController.login)
