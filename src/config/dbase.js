70d08dac88295e5bc4bbb49f4fe35e546561bc2a
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-05-13 20:24:30 +0000

    fix

--- src/config/dbase.js
+++ src/config/dbase.js
@@ -40,16 +40,32 @@ const Sequelize = require("sequelize");
 let sequelize;
 
 if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize('production', 'ishimwe', 'Password@123', {
-    host: 'db',
-    port: 3306,
-    dialect: 'mysql',
+  sequelize = new Sequelize('development', 'avnadmin', 'AVNS_EfUj-VLFHelXr_GugKm', {
+    host: 'test-robertishimwe0-5cb2.a.aivencloud.com',
+    port: 26939,
+    dialect:'mysql',
+    dialectOptions: {
+      ssl: {
+        ca: path.join(__dirname, 'ca.pem'),
+        rejectUnauthorized: false,
+      }
+    },
     pool: {
       max: 40,
       min: 0,
       idle: 1000000
     },
   });
+  // sequelize = new Sequelize('production', 'ishimwe', 'Password@123', {
+  //   host: 'db',
+  //   port: 3306,
+  //   dialect: 'mysql',
+  //   pool: {
+  //     max: 40,
+  //     min: 0,
+  //     idle: 1000000
+  //   },
+  // });
 } else {
   sequelize = new Sequelize('development', 'avnadmin', 'AVNS_EfUj-VLFHelXr_GugKm', {
     host: 'test-robertishimwe0-5cb2.a.aivencloud.com',
